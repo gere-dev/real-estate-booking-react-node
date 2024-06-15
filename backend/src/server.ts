bd634581f61ab93e1bf374349c7c826343189a63
gere-dev <gere.dev@protonmail.com>
2024-06-15 15:59:31 -0400

    implement the register route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import routes from '@/routes';
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.static('dist'));
+
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
