85dabd86dd76b26df1b501e222a2e7b2ae27aeb3
gere-dev <gere.dev@protonmail.com>
2024-06-15 17:10:40 -0400

    add register controller

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -1,19 +1,44 @@
 import db from '@/database/config/db';
+import { generateAccessToken, generateRefreshToken } from '@/utils/jwt.utils';
 import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
+import { ResultSetHeader } from 'mysql2';
 
 export const register = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
 
+    // hash the password
     const SALT = 10;
     const hashedPassword = await bcrypt.hash(password, SALT);
 
+    // insert into database
     const values = [name, email, hashedPassword];
+    const [result] = await db.query<ResultSetHeader>('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', values);
 
-    // await db.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', values);
+    // generate access token and refresh token
+    const userId = result.insertId;
+    const accessToken = generateAccessToken(userId);
+    const refreshToken = generateRefreshToken(userId);
 
-    res.status(200).json(values);
+    // save refresh token in database
+    await db.query<ResultSetHeader>('INSERT INTO refresh_tokens (user_id, token) VALUES (?, ?)', [userId, refreshToken]);
+
+    // set cookie
+    res.cookie('access_token', accessToken, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 1000, //24 hours in milliseconds
+      secure: true,
+      sameSite: 'none',
+    });
+
+    const user = {
+      id: userId,
+      name,
+      email,
+    };
+
+    res.status(201).json(user);
   } catch (error) {
     console.log(`Error at register controller: ${error}`);
     res.status(500).json({ message: 'Internal server error' });
