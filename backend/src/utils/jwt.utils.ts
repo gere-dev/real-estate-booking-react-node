7e0c8c7de98f7c7d50bae89e0494a50e68feedaa
gere-dev <gere.dev@protonmail.com>
2024-06-15 21:32:27 -0400

    refactor the token generation function

--- backend/src/utils/jwt.utils.ts
+++ backend/src/utils/jwt.utils.ts
@@ -1,14 +1,13 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { ACCESS_TOKEN_MAX_AGE, REFRESH_TOKEN_MAX_AGE } from '@/constants/tokens.max.age';
 
 dotenv.config();
 
-const ACCESS_TOKEN_LIFETIME = '120s';
-const REFRESH_TOKEN_LIFETIME = '60s';
 const SECRET_KEY: string = process.env.JWT_SECRET as string;
 
-const generateToken = (id: number, secret: string, lifeTime: string) => {
-  const token = jwt.sign({ id }, secret as string, {
+const generateToken = (id: number, secret_key: string, lifeTime: number) => {
+  const token = jwt.sign({ id }, secret_key as string, {
     expiresIn: lifeTime,
   });
 
@@ -16,16 +15,16 @@ const generateToken = (id: number, secret: string, lifeTime: string) => {
 };
 
 export const generateAccessToken = (id: number): string => {
-  return generateToken(id, SECRET_KEY, ACCESS_TOKEN_LIFETIME);
+  return generateToken(id, SECRET_KEY, ACCESS_TOKEN_MAX_AGE);
 };
 
 export const generateRefreshToken = (id: number): string => {
-  return generateToken(id, SECRET_KEY, REFRESH_TOKEN_LIFETIME);
+  return generateToken(id, SECRET_KEY, REFRESH_TOKEN_MAX_AGE);
 };
 
 export const verifyToken = async (token: string): Promise<any> => {
   try {
-    const decoded = await jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET as string);
+    const decoded = await jwt.verify(token.replace('Bearer ', ''), SECRET_KEY as string);
     return decoded;
   } catch (error) {
     throw new Error('token verification failed');
