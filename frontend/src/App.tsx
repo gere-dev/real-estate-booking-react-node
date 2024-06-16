decc268445fe228b40b4906683bbfa2725ee7322
gere-dev <gere.dev@protonmail.com>
2024-06-15 23:21:26 -0400

    setup react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Nav } from '@/components';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Nav />
+    </Routes>
+  );
+}
+
+export default App;
