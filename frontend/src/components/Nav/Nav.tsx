4a07728e02e494af33ef2df505a24bb67fd15190
gere-dev <gere.dev@protonmail.com>
2024-06-16 09:24:16 -0400

    remove unused code

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -5,12 +5,12 @@ import { NavList } from './NavList';
 
 export const Nav = () => {
   return (
-    <header className='border'>
+    <header>
       <div className='flex justify-between items-center px-10  border-b py-2'>
         <Link className='bg-primary text-white py-1 px-3 rounded-full' to={'/'}>
           B
         </Link>
-        <nav className=''>
+        <nav>
           <ul className='flex gap-5  '>
             {navs.map((nav) => {
               return <NavList nav={nav} key={nav.title} />;
