f067d38c29623d66d1fa37ad45f2a78338245d1c
gere-dev <gere.dev@protonmail.com>
2024-06-24 21:02:36 -0400

    save the nav height as a constant

--- frontend/src/components/Auth/AuthContainer.tsx
+++ frontend/src/components/Auth/AuthContainer.tsx
@@ -1,10 +1,12 @@
+import { NAV_HEIGHT } from '@/constants';
 import React from 'react';
 
 interface Props {
   children: React.ReactNode;
 }
+
 export const AuthContainer: React.FC<Props> = ({ children }) => {
-  return <section className='flex flex-col'>{children}</section>;
+  return <section className={`flex flex-col justify-center h-[calc(100vh-${NAV_HEIGHT}px)]`}> {children}</section>;
 };
 
 export default AuthContainer;
