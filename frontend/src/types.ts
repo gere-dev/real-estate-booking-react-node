2f52b32b0cfcba07461bd9548e82d2b92d32671f
gere-dev <gere.dev@protonmail.com>
2024-06-27 19:55:28 -0400

    add property type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -3,3 +3,19 @@ export interface AuthForm {
   email: string;
   password: string;
 }
+
+export interface Property {
+  property_id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  location: string;
+  user_id: number;
+  wifi: boolean;
+  parking: boolean;
+  pets: boolean;
+  gym: boolean;
+  pool: boolean;
+  netflix: boolean;
+}
