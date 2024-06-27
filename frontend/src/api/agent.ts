2665a7e9a9fe4f69739e37125702e6c063b97777
gere-dev <gere.dev@protonmail.com>
2024-06-27 19:58:00 -0400

    create a centralized crud operations using axios.  for the properties

---
+++ frontend/src/api/agent.ts
@@ -0,0 +1,29 @@
+import { Property } from '@/types';
+import axios, { AxiosResponse } from 'axios';
+
+const apiUrl = process.env.NODE === 'production' ? process.env.REACT_APP_API_URL : 'http://localhost:5000/api';
+
+axios.defaults.baseURL = apiUrl;
+axios.defaults.withCredentials = true;
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: object) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: object) => axios.put(url, body).then(responseBody),
+  delete: (url: string) => axios.delete(url).then(responseBody),
+};
+
+const Properties = {
+  list: () => requests.get('/properties'),
+  create: (property: Property) => requests.post(`/properties/`, property),
+  update: (property: Property) => requests.put(`/properties/${property.property_id}`, property),
+  delete: (propertyId: number) => requests.delete(`/properties/${propertyId}`),
+};
+
+const agent = {
+  Properties,
+};
+
+export default agent;
