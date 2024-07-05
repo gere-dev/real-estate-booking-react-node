fb89eaee9bfa809a1ec76d17f5e26ba04a7e67e5
gere-dev <gere.dev@protonmail.com>
2024-07-05 13:46:30 -0400

    add redux persistor dependency

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,5 +1,9 @@
 import propertiesSlice from './properties/propertiesSlice';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+//Reducers
 import propertySlice from './property/propertySlice';
 import authSlice from './auth/authSlice';
 import ListingsSlice from './Listings/ListingsSlice';
@@ -10,9 +14,21 @@ const rootReducer = combineReducers({
   auth: authSlice,
   listings: ListingsSlice,
 });
+// Persistor
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
