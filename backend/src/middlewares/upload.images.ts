0c52268d6aae4cb1feac7c67adbe072a2079a8ec
gere-dev <gere.dev@protonmail.com>
2024-07-04 21:11:14 -0400

    implement upload middleware

---
+++ backend/src/middlewares/upload.images.ts
@@ -0,0 +1,15 @@
+import multer from 'multer';
+import path from 'path';
+
+const uploadPath = path.join(__dirname, 'src', 'uploads');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '_' + file.originalname);
+  },
+});
+
+export const upload = multer({ storage: storage });
--- backend/src/routes/listings.route.ts
+++ backend/src/routes/listings.route.ts
@@ -1,7 +1,9 @@
-import { getListings } from '@/controllers';
+import { AddListing, getListings } from '@/controllers';
+import { upload } from '@/middlewares';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/', getListings);
+router.post('/add-listing', upload.array('images'), AddListing);
 export default router;
