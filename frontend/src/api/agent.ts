92edb7d4b7ebd3a1d7c626913500035ec4e317cb
gere-dev <gere.dev@protonmail.com>
2024-07-07 12:32:27 -0400

    create an instance of private and public axios requests. and refactor agent

--- frontend/src/api/agent.ts
+++ frontend/src/api/agent.ts
@@ -1,46 +1,67 @@
 import { logout } from '@/state/auth/authSlice';
 import store from '@/state/store';
 import { Login, Register, Property as PropertyType, NewProperty } from '@/types';
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 export const apiUrl = import.meta.env.NODE === 'production' ? import.meta.env.VITE_API_URL : 'http://localhost:5000/api';
 
-axios.defaults.baseURL = apiUrl;
-axios.defaults.withCredentials = true;
+export const privateInstance: AxiosInstance = axios.create({
+  baseURL: apiUrl,
+  withCredentials: true,
+});
+
+export const publicInstance: AxiosInstance = axios.create({
+  baseURL: apiUrl,
+});
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
-  post: (url: string, body: object) => axios.post(url, body).then(responseBody),
-  put: (url: string, body: object) => axios.put(url, body).then(responseBody),
-  delete: (url: string) => axios.delete(url).then(responseBody),
-};
+// const publicRequests = {
+//   get: (url: string) => publicInstance.get(url).then(responseBody),
+//   post: (url: string, body: object) => publicInstance.post(url, body).then(responseBody),
+//   put: (url: string, body: object) => publicInstance.put(url, body).then(responseBody),
+//   delete: (url: string) => publicInstance.delete(url).then(responseBody),
+// };
+// const privateRequests = {
+//   get: (url: string) => privateInstance.get(url).then(responseBody),
+//   post: (url: string, body: object) => privateInstance.post(url, body).then(responseBody),
+//   put: (url: string, body: object) => privateInstance.put(url, body).then(responseBody),
+//   delete: (url: string) => privateInstance.delete(url).then(responseBody),
+// };
+
+const createRequests = (instance: AxiosInstance) => ({
+  get: (url: string) => instance.get(url).then(responseBody),
+  post: (url: string, body: object) => instance.post(url, body).then(responseBody),
+  put: (url: string, body: object) => instance.put(url, body).then(responseBody),
+  delete: (url: string) => instance.delete(url).then(responseBody),
+});
+const privateRequests = createRequests(privateInstance);
+const publicRequests = createRequests(publicInstance);
 
 const Properties = {
-  list: () => requests.get('/properties'),
-  create: (property: PropertyType) => requests.post(`/properties/`, property),
-  update: (property: PropertyType) => requests.put(`/properties/${property.property_id}`, property),
-  delete: (propertyId: number) => requests.delete(`/properties/${propertyId}`),
+  list: () => privateRequests.get('/properties'),
+  create: (property: PropertyType) => privateRequests.post(`/properties/`, property),
+  update: (property: PropertyType) => privateRequests.put(`/properties/${property.property_id}`, property),
+  delete: (propertyId: number) => privateRequests.delete(`/properties/${propertyId}`),
 };
 
 const Property = {
-  get: (id: number) => requests.get(`/property/${id}`),
+  get: (id: number) => privateRequests.get(`/property/${id}`),
 };
 
 const Listings = {
-  list: () => requests.get('/listings'),
-  create: (listing: NewProperty) => requests.post(`/listings`, listing),
-  update: (listing: PropertyType) => requests.put(`/listings/${listing.property_id}`, listing),
-  delete: (propertyId: number) => requests.delete(`/listings/${propertyId}`),
-  getById: (id: number) => requests.get(`/listings/${id}`),
+  list: () => privateRequests.get('/listings'),
+  create: (listing: NewProperty) => privateRequests.post(`/listings`, listing),
+  update: (listing: PropertyType) => privateRequests.put(`/listings/${listing.property_id}`, listing),
+  delete: (propertyId: number) => privateRequests.delete(`/listings/${propertyId}`),
+  getById: (id: number) => privateRequests.get(`/listings/${id}`),
 };
 
 const Auth = {
-  register: (data: Register) => requests.post(`/auth/register`, data),
-  login: (data: Login) => requests.post(`/auth/login`, data),
-  logout: () => requests.post(`/auth/logout`, {}),
-  refresh: () => requests.post(`/auth/refresh`, {}),
+  register: (data: Register) => publicRequests.post(`/auth/register`, data),
+  login: (data: Login) => publicRequests.post(`/auth/login`, data),
+  logout: () => publicRequests.post(`/auth/logout`, {}),
+  refresh: () => publicRequests.post(`/auth/refresh`, {}),
 };
 
 const agent = {
