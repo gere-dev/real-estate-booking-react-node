3f96d6a8c54d08bbf2f25700c1fa7330289b3a3c
gere-dev <gere.dev@protonmail.com>
2024-07-10 16:06:26 -0400

    implement routing for the update listings

--- backend/src/routes/listings.route.ts
+++ backend/src/routes/listings.route.ts
@@ -1,4 +1,4 @@
-import { createListing, getListings } from '@/controllers';
+import { createListing, getListings, updateListing } from '@/controllers';
 import { upload } from '@/middlewares';
 import { Router } from 'express';
 
@@ -6,4 +6,5 @@ const router = Router();
 
 router.get('/fetch', getListings);
 router.post('/create', upload.array('images'), createListing);
+router.put('/update-listings/:propertyId', upload.array('images'), updateListing);
 export default router;
