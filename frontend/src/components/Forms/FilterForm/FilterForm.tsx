bfb63bd7375605973db78d3f68b58bceea7445ce
gere-dev <gere.dev@protonmail.com>
2024-07-12 21:16:54 -0400

    delete the FilterFormContainer. implement the onchange events for the location, prince range and beds of FilterForm

--- frontend/src/components/Forms/FilterForm/FilterForm.tsx
+++ frontend/src/components/Forms/FilterForm/FilterForm.tsx
@@ -1,33 +1,54 @@
 import { BiBed, BiDollar, BiSearch } from 'react-icons/bi';
-import { FieldContainer, SearchButton, FormContainer } from '@/components';
+import { FieldContainer, SearchButton } from '@/components';
+import { ComponentProps, useState } from 'react';
 
 export const FilterForm = () => {
+  const [formData, setFormData] = useState({
+    location: '',
+    price: '',
+    bed: '',
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSummit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <FormContainer>
-      <FieldContainer Icon={BiSearch} children={<LocationInput />} htmlFor='location' label='Search by Location' />
-      <FieldContainer Icon={BiDollar} children={<PriceRangeInput />} htmlFor='price' label='Price Range' />
-      <FieldContainer Icon={BiBed} children={<NumOfBedSelect />} htmlFor='bed' label='Number of Beds' />
+    <form
+      onSubmit={handleSummit}
+      className='flex flex-col gap-4 lg:flex-row lg:items-center lg:shadow-lg lg:-mt-16 rounded-full py-2 lg:px-8 z-10 lg:h-fit bg-white'
+    >
+      <FieldContainer Icon={BiSearch} children={<LocationInput onChange={handleChange} />} htmlFor='location' label='Search by Location' />
+      <FieldContainer Icon={BiDollar} children={<PriceRangeInput onChange={handleChange} />} htmlFor='price' label='Price Range' />
+      <FieldContainer Icon={BiBed} children={<NumOfBedSelect onChange={handleChange} />} htmlFor='bed' label='Number of Beds' />
       <SearchButton />
-    </FormContainer>
+    </form>
   );
 };
 
-const LocationInput = () => {
-  return <input id='location' className='pr-2 flex-1 outline-none w-full' placeholder='Location...' type='text' name='location' />;
+const LocationInput = ({ ...props }: ComponentProps<'input'>) => {
+  return <input {...props} id='location' className='pr-2 flex-1 outline-none w-full' placeholder='Location...' type='text' name='location' />;
 };
 
-const PriceRangeInput = () => {
+const PriceRangeInput = ({ ...props }: ComponentProps<'input'>) => {
   return (
     <input
+      {...props}
+      id='price'
       className='flex-1 appearance-none w-full h-2 bg-primary bg-opacity-30 rounded-lg outline-none focus:outline-none hover:cursor-pointer range-input'
       type='range'
       name='price'
     />
   );
 };
-const NumOfBedSelect = () => {
+const NumOfBedSelect = ({ ...props }: ComponentProps<'select'>) => {
   return (
-    <select className='flex-1 bg-transparent border-none w-full ' name='bed' id='number of Beds'>
+    <select {...props} id='bed' className='flex-1 bg-transparent border-none w-full ' name='bed'>
       <option value='any'>Any</option>
       <option value='1'>1</option>
       <option value='2'>2</option>
--- frontend/src/components/Forms/FilterForm/index.ts
+++ frontend/src/components/Forms/FilterForm/index.ts
@@ -2,4 +2,3 @@ export * from './IconContainer';
 export * from './FilterForm';
 export * from './FieldContainer';
 export * from './SearchButton';
-export * from './FormContainer';
