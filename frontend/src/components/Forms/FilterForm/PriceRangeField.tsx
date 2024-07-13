a2a882b7a23eba0a802f971eb81d584aa8ac4805
gere-dev <gere.dev@protonmail.com>
2024-07-13 15:20:16 -0400

    install  material ui and use the mui slider for the price range

--- frontend/src/components/Forms/FilterForm/PriceRangeField.tsx
+++ frontend/src/components/Forms/FilterForm/PriceRangeField.tsx
@@ -1,16 +1,32 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, useState } from 'react';
 import { FieldContainer } from './FieldContainer';
 import { BiDollar } from 'react-icons/bi';
+import { Slider } from '@mui/material';
 
 export const PriceRangeField = ({ ...props }: ComponentProps<'input'>) => {
+  const [value, setValue] = useState<number[]>([20, 100]);
+
+  const minDistance = 20;
+
+  const handleChange = (event: Event, newValue: number | number[], activeThumb: number) => {
+    if (!Array.isArray(newValue)) return;
+
+    if (activeThumb === 0) {
+      setValue([Math.min(newValue[0], value[1] - minDistance), value[1]]);
+    } else {
+      setValue([value[0], Math.max(newValue[1], value[0] + minDistance)]);
+    }
+  };
+
   return (
     <FieldContainer Icon={BiDollar} htmlFor='price' label='Price Range'>
-      <input
-        {...props}
-        id='price'
-        className='flex-1 appearance-none w-full h-2 bg-primary bg-opacity-30 rounded-lg outline-none focus:outline-none hover:cursor-pointer range-input'
-        type='range'
-        name='price'
+      <Slider
+        getAriaLabel={() => 'Minimum distance'}
+        value={value}
+        onChange={handleChange}
+        valueLabelDisplay='auto'
+        disableSwap
+        sx={{ color: ' #ff385c ' }}
       />
     </FieldContainer>
   );
