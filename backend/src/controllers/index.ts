24e4110d14900c124dec36cbb4a7423a3f1220da
gere-dev <gere.dev@protonmail.com>
2024-07-13 21:25:27 -0400

    delete the property controller and router.

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,6 +1,3 @@
 export * from './auth.controllers';
 export * from './properties.controller';
-
-export * from './property.controller';
-
 export * from './listings.controller';
--- backend/src/routes/property.route.ts
+++
@@ -1,8 +0,0 @@
-import { filterProperties, getProperty } from '@/controllers';
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/fetch/:id', getProperty);
-
-export default router;
