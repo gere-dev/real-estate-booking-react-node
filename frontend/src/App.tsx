98885a1fc387bbef27f8f57131bb40a7b3f14e66
gere-dev <gere.dev@protonmail.com>
2024-07-13 22:54:07 -0400

    add the filter page to the Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Nav, PrivateRoute, Property } from '@/components';
 import { Home, Login, Register, Bookings, AddListing, Listings, NotFound, EditListings } from '@/pages';
 import './App.css';
+import { FilteredProperties } from './pages/FilteredProperties/FilteredProperties';
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/property/:id' element={<Property />} />
+        <Route path='/filtered-properties' element={<FilteredProperties />} />
         <Route element={<PrivateRoute />}>
           <Route path='/account/:userId' element={<Listings />} />
           <Route path='/account/bookings' element={<Bookings />} />
