8c9630597f7148e393ee55901c2e39ac2f046003
gere-dev <gere.dev@protonmail.com>
2024-07-15 20:12:32 -0400

    update the bookings types

--- frontend/src/types/Booking.type.ts
+++ frontend/src/types/Booking.type.ts
@@ -1,14 +1,10 @@
-export interface Booking {
-  id: number;
+import { Property } from './property.type';
+
+export interface Booking extends Property {
+  booking_id: number;
   property_id: number;
   user_id: number;
   start_date: string;
   end_date: string;
   total_price: number;
-  status: string;
-  image_url: string[] | [];
-  title: string;
-  city: string;
-  state: string;
-  bed: number;
 }
