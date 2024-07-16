a20f938b3bc1ec359b15712a9a21e94d5dfeb6d3
gere-dev <gere.dev@protonmail.com>
2024-07-16 10:01:53 -0400

    calculate all the booking form costs and use the useMemo to memoize the values and optimize the app

--- frontend/src/components/Forms/BookingForm/BookingDateInput.tsx
+++ frontend/src/components/Forms/BookingForm/BookingDateInput.tsx
@@ -5,14 +5,16 @@ interface Props {
   htmlFor: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
+  value: string;
+  min?: string;
 }
-export const BookingDateInput: React.FC<Props> = ({ label, htmlFor, onChange, className }) => {
+export const BookingDateInput: React.FC<Props> = ({ label, htmlFor, onChange, className, value, min }) => {
   return (
     <div className={`flex flex-col font-bold flex-1 p-2  ${className}`}>
       <label className='text-sm' htmlFor={htmlFor}>
         {label}
       </label>
-      <input name={htmlFor} onChange={onChange} id={htmlFor} className='font-normal text-sm outline-none' type='date' />
+      <input min={min} value={value} name={htmlFor} onChange={onChange} id={htmlFor} className='font-normal text-sm outline-none' type='date' />
     </div>
   );
 };
--- frontend/src/components/Forms/BookingForm/BookingForm.tsx
+++ frontend/src/components/Forms/BookingForm/BookingForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { BookingDateInput, BookingPriceSummary, RectangleButton } from '@/components';
 
 interface Props {
@@ -7,6 +7,31 @@ interface Props {
   bed: number;
 }
 const BookingForm: React.FC<Props> = ({ price, propertyId, bed }) => {
+  const today = new Date().toISOString().split('T')[0];
+  const [formData, setFormData] = React.useState<{ checkin: string; checkout: string; guest: string }>({
+    checkin: today,
+    checkout: '',
+    guest: '1',
+  });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const { checkin, checkout, guest } = formData;
+
+  const { days, cost_per_guest_night, cleaning_fee, service_fee, tax, total } = useMemo(() => {
+    const days = checkin && checkout ? Math.max(((new Date(checkout).getTime() - new Date(checkin).getTime()) / (1000 * 60 * 60 * 24)) | 0, 0) : 0;
+    const cost_per_guest_night = price * Number(guest) * days;
+    const cleaning_fee = 0.1 * cost_per_guest_night;
+    const service_fee = 0.05 * cost_per_guest_night;
+    const tax = (cost_per_guest_night + cleaning_fee + service_fee) * 0.13;
+    const total = cost_per_guest_night + cleaning_fee + service_fee + tax;
+
+    return { days, cost_per_guest_night, cleaning_fee, service_fee, tax, total };
+  }, [checkin, checkout, guest, price]);
+
   return (
     <div className=' shadow-lg p-3 rounded-lg'>
       <span className='font-bold text-lg'>
@@ -16,23 +41,38 @@ const BookingForm: React.FC<Props> = ({ price, propertyId, bed }) => {
       <form>
         <div className='border flex flex-col gap-2 mb-2 rounded-md'>
           <div className='flex  border-b'>
-            <BookingDateInput label='CHECK-IN' htmlFor='checkin' onChange={() => null} className='border-r' />
-            <BookingDateInput label='CHECK-OUT' htmlFor='checkout' onChange={() => null} />
+            <BookingDateInput
+              min={new Date().toISOString().split('T')[0]}
+              value={formData.checkin}
+              label='CHECK-IN'
+              htmlFor='checkin'
+              onChange={handleChange}
+              className='border-r'
+            />
+            <BookingDateInput min={formData.checkin} value={formData.checkout} label='CHECK-OUT' htmlFor='checkout' onChange={handleChange} />
           </div>
           <div className='flex flex-col  p-2'>
             <span className='font-semibold text-sm'>Number Of Guests</span>
-            <input className='border py-1 outline-none px-2' type='number' min={1} max={bed} />
+            <input
+              value={Number(formData.guest) <= bed ? formData.guest : bed}
+              onChange={handleChange}
+              name='guest'
+              className='border py-1 outline-none px-2'
+              type='number'
+              min={1}
+              max={bed}
+            />
           </div>
         </div>
         <RectangleButton label='Reserve' />
       </form>
       <div className='my-4'>
-        <BookingPriceSummary title='$100 X guest(s) X 0 night(s)' price={price} />
-        <BookingPriceSummary title='Cleaning fee' price={price} />
-        <BookingPriceSummary title='Service fee' price={price} />
-        <BookingPriceSummary title='Taxes' price={price} />
+        <BookingPriceSummary title={`$100 X guest(s) X ${days} night(s)`} price={cost_per_guest_night} />
+        <BookingPriceSummary title='Cleaning fee' price={cleaning_fee} />
+        <BookingPriceSummary title='Service fee' price={service_fee} />
+        <BookingPriceSummary title='Taxes' price={tax} />
         <hr />
-        <BookingPriceSummary title='Total' price={price} />
+        <BookingPriceSummary title='Total' price={total} />
       </div>
     </div>
   );
