f89a6987bb70a33617f1260c7c3bf47fb6d410de
gere-dev <gere.dev@protonmail.com>
2024-07-18 09:36:35 -0400

    validate the register form before dispatching

--- frontend/src/pages/Auth/Register/Register.tsx
+++ frontend/src/pages/Auth/Register/Register.tsx
@@ -1,9 +1,10 @@
 import { AuthForm, AuthMessageLink, Title, AuthContainer } from '@/components';
-import { Register as RegisterType, Login } from '@/types';
+import { Register as RegisterType, Login, AuthFormErrors } from '@/types';
 import { AuthForm as AuthFormEnum, authMessageLinkProps } from '@/enums';
 import { useState } from 'react';
 import { useAppDispatch } from '@/state/hooks';
 import { register } from '@/state/auth/authSlice';
+import { validateRegisterForm } from '@/utils/validation';
 export const Register = () => {
   const [formData, setFormData] = useState<RegisterType>({
     name: '',
@@ -11,13 +12,24 @@ export const Register = () => {
     password: '',
   });
 
+  const [errors, setErrors] = useState<AuthFormErrors>();
+
   const dispatch = useAppDispatch();
 
+  // Handles form submission for user registration.
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch(register(formData));
+
+    // Validate form data before dispatching registration action.
+    const validateErrors = validateRegisterForm(formData);
+    if (Object.keys(validateErrors).length === 0) {
+      dispatch(register(formData));
+    } else {
+      setErrors(validateErrors);
+    }
   };
 
+  // Updates form data in response to user input changes.
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
--- frontend/src/utils/validation.ts
+++ frontend/src/utils/validation.ts
@@ -15,3 +15,20 @@ export const validateLoginForm = (formData: Login): AuthFormErrors => {
   }
   return errors;
 };
+
+export const validateRegisterForm = (formData: Register): AuthFormErrors => {
+  const errors: AuthFormErrors = {};
+  if (!formData.name) {
+    errors.name = 'Name is required';
+  }
+  if (!formData.email) {
+    errors.email = 'Email is required';
+  }
+  if (!pattern.test(formData.email)) {
+    errors.email = 'Invalid email';
+  }
+  if (!formData.password) {
+    errors.password = 'Password is required';
+  }
+  return errors;
+};
