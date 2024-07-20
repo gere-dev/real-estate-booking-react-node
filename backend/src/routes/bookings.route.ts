1a059dd9d2fbbdc651c44b2809c0ef7b47ebc8c3
gere-dev <gere.dev@protonmail.com>
2024-07-20 05:00:31 -0400

    apply authenticateUser middleware globally before protected

--- backend/src/routes/bookings.route.ts
+++ backend/src/routes/bookings.route.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import { createBooking, deleteBooking, getAllBookings } from '@/controllers';
-import { authenticateUser } from '@/middlewares';
 import { END_POINTS } from '@/constants';
 
 const router = Router();
 const endpoints = END_POINTS.bookings;
-router.get(endpoints.getAll, authenticateUser, getAllBookings);
-router.post(endpoints.create, authenticateUser, createBooking);
-router.delete(endpoints.delete, authenticateUser, deleteBooking);
+router.get(endpoints.getAll, getAllBookings);
+router.post(endpoints.create, createBooking);
+router.delete(endpoints.delete, deleteBooking);
 export default router;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,11 +3,18 @@ import authRoutes from './auth.routes';
 import propertiesRoutes from './properties.route';
 import listingsRoutes from './listings.route';
 import bookingsRoutes from './bookings.route';
+import { authenticateUser } from '@/middlewares';
 
 const router = Router();
 
+// public routes
 router.use('/auth', authRoutes);
 router.use('/properties', propertiesRoutes);
+
+// apply authenticateUser middleware globally before protected routes
+router.use(authenticateUser);
+
+// protected routes
 router.use('/listings', listingsRoutes);
 router.use('/bookings', bookingsRoutes);
 
