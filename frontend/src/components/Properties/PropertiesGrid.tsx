2bfed7e27a434279fb743acb400ef914b050933d
gere-dev <gere.dev@protonmail.com>
2024-07-21 13:32:07 -0400

    remove the loading component from the PropertiesGrid component

--- frontend/src/components/Properties/PropertiesGrid.tsx
+++ frontend/src/components/Properties/PropertiesGrid.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Loading, PropertiesCard } from '@/components';
+import { PropertiesCard } from '@/components';
 import { Booking, Property } from '@/types';
 
 interface Props {
-  properties: Property[] | Booking[] | null;
+  properties: Property[] | Booking[];
 }
 export const PropertiesGrid: React.FC<Props> = ({ properties }) => {
-  if (!properties) {
-    return <Loading />;
-  }
   return (
     <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 flex-1'>
       {properties.map((property) => (
