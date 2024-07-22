e0f77466a6e4c52c1465ea2658a563e8ef9a59be
gere-dev <gere.dev@protonmail.com>
2024-07-21 20:37:21 -0400

    fix the tsconfig paths. add tsconfigPaths plugin to vite.config  and remove all the alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@components': resolve(__dirname, 'src/components'),
-      '@pages': resolve(__dirname, 'src/pages'),
-    },
   },
 });
