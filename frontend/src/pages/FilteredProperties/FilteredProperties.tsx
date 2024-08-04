9cc55ad851b10ccf24fe23c2fba52db30ccb9488
gere-dev <gere.dev@protonmail.com>
2024-08-03 20:05:09 -0400

    scroll to the top when FilteredProperties mounts

--- frontend/src/pages/FilteredProperties/FilteredProperties.tsx
+++ frontend/src/pages/FilteredProperties/FilteredProperties.tsx
@@ -1,6 +1,6 @@
 import { PropertiesGrid } from '@/components';
 import { selectFilteredProperties } from '@/state/filterProperties/filterPropertiesSelectors';
-import { useAppSelector } from '@/hooks';
+import { useAppSelector, useScrollTop } from '@/hooks';
 import { BiArrowBack } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,9 +8,12 @@ export const FilteredProperties = () => {
   const properties = useAppSelector(selectFilteredProperties);
   const navigate = useNavigate();
 
+  //scroll to the top when it mounts
+  useScrollTop();
+
   return (
     <section className='max-width-container py-2'>
-      <button onClick={() => navigate(-1)} className='text-2xl text-white bg-primary rounded-full p-1'>
+      <button onClick={() => navigate(-1)} className='text-2xl text-white bg-primary rounded-full p-1 my-3'>
         <BiArrowBack />
       </button>
       <PropertiesGrid properties={properties} />
