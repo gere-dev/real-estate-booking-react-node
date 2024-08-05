c5019ed7e08388c3f0f3e1022f57a811712b4274
gere-dev <gere.dev@protonmail.com>
2024-08-05 19:02:44 -0400

    redirect users to home page or to the path they came from after successfully registering

--- frontend/src/pages/Auth/Register/Register.tsx
+++ frontend/src/pages/Auth/Register/Register.tsx
@@ -1,11 +1,13 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { AuthForm, AuthMessageLink, Title, AuthContainer } from '@/components';
-import { Register as RegisterType, AuthFormErrors } from '@/types';
-import { useState } from 'react';
-import { useAppDispatch } from '@/hooks';
+import { Register as RegisterType, AuthFormErrors, Status } from '@/types';
+import { useAppDispatch, useAppSelector } from '@/hooks';
 import { validateRegisterForm } from '@/utils';
-import { register } from '@/state';
+import { register, selectAuthStatus } from '@/state';
 import { COMPONENTS_DATA } from '@/constants';
 export const Register = () => {
+  //State
   const [formData, setFormData] = useState<RegisterType>({
     name: '',
     email: '',
@@ -14,9 +16,23 @@ export const Register = () => {
 
   const [errors, setErrors] = useState<AuthFormErrors>();
 
+  //Constants
   const { loginPage, registerPage } = COMPONENTS_DATA;
 
+  //Hooks
+  const location = useLocation();
   const dispatch = useAppDispatch();
+  const status = useAppSelector(selectAuthStatus);
+  const navigate = useNavigate();
+
+  // Redirect on successful login
+  const from = location.state?.from?.pathname || '/';
+  useEffect(() => {
+    if (status === Status.SUCCEEDED) {
+      const redirectTo = from === '/login' || from === '/register' ? '/' : from;
+      navigate(redirectTo, { replace: true });
+    }
+  }, [status, navigate, from]);
 
   // Handles form submission for user registration.
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
