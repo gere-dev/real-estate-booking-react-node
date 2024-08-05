551e414170d3afeca4bdd9b369ed84b71cf307b0
gere-dev <gere.dev@protonmail.com>
2024-08-05 18:53:24 -0400

    remove the auth provider and use the useLocation hook to redirect users to where they came from after logging in

--- frontend/src/pages/Auth/Login/Login.tsx
+++ frontend/src/pages/Auth/Login/Login.tsx
@@ -1,34 +1,39 @@
-import { AuthForm, AuthMessageLink, Title, AuthContainer, AuthProvider } from '@/components';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthForm, AuthMessageLink, Title, AuthContainer } from '@/components';
 import { selectAuthStatus, login } from '@/state';
 import { useAppDispatch, useAppSelector } from '@/hooks';
 import { AuthFormErrors, Login as LoginType, Status } from '@/types';
 import { validateLoginForm } from '@/utils';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { COMPONENTS_DATA } from '@/constants';
 
 export const Login = () => {
+  //State
   const [formData, setFormData] = useState<LoginType>({
     email: '',
     password: '',
   });
   const [errors, setErrors] = useState<AuthFormErrors>();
 
+  //Constants
   const { loginPage, registerPage } = COMPONENTS_DATA;
 
+  //Hooks
+  const location = useLocation();
   const dispatch = useAppDispatch();
   const status = useAppSelector(selectAuthStatus);
   const navigate = useNavigate();
 
-  const intendedDestination = sessionStorage.getItem('intendedDestination');
-
+  // Redirect on successful login
+  const from = location.state?.from?.pathname || '/';
   useEffect(() => {
     if (status === Status.SUCCEEDED) {
-      navigate(intendedDestination || '/');
-      sessionStorage.removeItem('intendedDestination');
+      const redirectTo = from === '/login' || from === '/register' ? '/' : from;
+      navigate(redirectTo, { replace: true });
     }
-  }, [status, navigate, intendedDestination]);
+  }, [status, navigate, from]);
 
+  //Event Handlers
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -44,12 +49,12 @@ export const Login = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
   return (
-    <AuthProvider>
+    <>
       <AuthContainer>
         <Title title={loginPage.title} />
         <AuthForm onSubmit={handleSubmit} onChange={handleChange} />
         <AuthMessageLink to={registerPage.link} linkText={registerPage.title} message={registerPage.message} />
       </AuthContainer>
-    </AuthProvider>
+    </>
   );
 };
